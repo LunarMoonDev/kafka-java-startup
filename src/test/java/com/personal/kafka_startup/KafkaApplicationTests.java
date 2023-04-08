a9fcbecf1d16117a8689984b98495faf020d5cb6
root <root@LAPTOP-HFJ5GN75>
2023-04-08 21:52:50 +0800

    feat: added spring boilerplates

---
+++ src/test/java/com/personal/kafka_startup/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.kafka_startup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
